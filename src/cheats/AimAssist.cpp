8b3d99123526c89c166fe7e8f9f77e1a819de644
SomePineaple <njweb@protonmail.com>
2022-02-04 14:06:07 -0500

    Increase AimAssist maximum speeds

--- src/cheats/AimAssist.cpp
+++ src/cheats/AimAssist.cpp
@@ -41,21 +41,16 @@ void AimAssist::run(Minecraft *mc) {
     if (!onlyOnClick || mouseState->buttonStates[1]) {
         float closestDistance = range;
 
-        std::cout << "Getting player container" << std::endl;
         EntityPlayerSP *thePlayer = mc->getPlayerContainer();
 
         EntityPlayer *closest = nullptr;
-        std::cout << "Getting players list" << std::endl;
         JavaList *players = mc->getWorldContainer()->getPlayers();
         if (players == nullptr)
             return;
 
-        std::cout << "Looping through players, number of players is: " << players->size() << std::endl;
         for (int i = 0; i < players->size(); i++) {
-            std::cout << "Getting player #" << i << std::endl;
             auto *player = new EntityPlayer(phantom, players->get(i));
-            std::cout << "Got player #" << i << " player id is " << player->getId() << std::endl;
-            if (player->getId() != mc->getPlayerContainer()->getId() && isInFOV(player, mc, fov)) {
+            if ((player->getId() != mc->getPlayerContainer()->getId()) && isInFOV(player, mc, fov)) {
                 auto newDist = (float) MathHelper::distance(player->getPosX(), player->getPosY(), player->getPosZ(), thePlayer->getPosX(), thePlayer->getPosY(), thePlayer->getPosZ());
                 if (newDist < closestDistance) {
                     closestDistance = newDist;
@@ -90,15 +85,14 @@ void AimAssist::run(Minecraft *mc) {
 void AimAssist::renderSettings() {
     ImGui::SliderFloat("AimAssist: range", &range, 0, 6, "%.2f");
     ImGui::SliderFloat("AimAssist: FOV", &fov, 0, 180, "%.1f");
-    ImGui::SliderFloat("AimAssist: hSpeed", &hSpeed, 0, 10, "%.2f");
-    ImGui::SliderFloat("AimAssist: vSpeed", &vSpeed, 0, 10, "%.2f");
+    ImGui::SliderFloat("AimAssist: hSpeed", &hSpeed, 0, 100, "%.2f");
+    ImGui::SliderFloat("AimAssist: vSpeed", &vSpeed, 0, 100, "%.2f");
     ImGui::Checkbox("AimAssist: Only while clicking", &onlyOnClick);
     ImGui::Checkbox("AimAssist: Center", &center);
     ImGui::Checkbox("AimAssist: Target Dead", &dead);
 }
 
 bool AimAssist::isInFOV(EntityPlayer *entity, Minecraft *mc, float fov) {
-    std::cout << "Checking if player with id " << entity->getId() << " is in the FOV" << std::endl;
     float playerYaw = MathHelper::wrapAngleTo180(mc->getPlayerContainer()->getRotationYaw()) + 180;
     float *targetRotations = MathHelper::getRotations(mc->getPlayerContainer(), entity);
     float targetYaw = targetRotations[0];
